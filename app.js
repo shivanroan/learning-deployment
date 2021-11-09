c91405d85c7527fc1483893daa53c83e77d05ad1
shivanroan <shivaruby46@gmail.com>
2021-11-09 23:24:42 +0530

    Main commit

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
 
+app.get('/',(req,res)=>{
+    res.status(200).send('THis is the home page da baka')
+})
+
 app.listen(9090,()=>{
     console.log("server is linking at port 9090");
 })
